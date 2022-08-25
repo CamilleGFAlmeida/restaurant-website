536c22dd831de90db8fb0c25d29a2d2a34861db1
CamilleGFAlmeida <97997891+CamilleGFAlmeida@users.noreply.github.com>
2022-08-25 20:54:26 -0300

    final updates

--- principal.js
+++ principal.js
@@ -1,3 +1,4 @@
+// Barra de Navegação 
 let menu = document.querySelector('#menu-icon'); 
 let navbar = document.querySelector('.navbar'); 
 
@@ -10,3 +11,37 @@ window.onscroll = () =>
 {
     navbar.classList.remove('active'); 
 }
+
+// Modo Dark 
+let darkmode = document.querySelector('#darkmode'); 
+
+darkmode.onclick = () => 
+{
+    if (darkmode.classList.contains('bx-moon'))
+    {
+        darkmode.classList.replace('bx-moon', 'bx-sun'); 
+        document.body.classList.add('active'); 
+    } 
+    else 
+    {
+        darkmode.classList.replace('bx-sun', 'bx-moon'); 
+        document.body.classList.remove('active'); 
+
+    }
+}
+
+// Scroll 
+    const sr = ScrollReveal ({
+            origin: 'top', 
+            distance: '40px', 
+            duration: 2000, 
+            reset: true 
+        }); 
+
+        sr.reveal(`.home-text, .home-img,
+            .about-img, .about-text,
+            .box, .s-box,
+            .btn, .connect-text,
+            .contact-box`, {
+    interval: 200
+})
